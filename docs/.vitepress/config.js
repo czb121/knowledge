b9d6034d147afb041074a7194fbe85292250a924
chenzhenbing <1377196580@qq.com>
2025-05-05 12:55:32 +0800

    feat: 新增配置

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,22 +2,29 @@
  * @Author: chenzhenbing 1377196580@qq.com
  * @Date: 2025-05-04 12:36:19
  * @LastEditors: chenzhenbing 1377196580@qq.com
- * @LastEditTime: 2025-05-04 12:36:31
+ * @LastEditTime: 2025-05-05 12:52:46
  * @FilePath: \study\docs\.vitepress\config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 export default {
+  title: '子非鱼', // 博客的标题
+  description: '', // 博客的介绍
+  base: '/blog/', // 如果想用 https://mlyz.wdy.github.io/blog/ 访问，那么这句话必填
   themeConfig: {
-    nav: [
-      { text: '关于', link: '/about' },
+    logo: "", // 页面上显示的logo
+    nav: [ // 页面右上角的导航
+      { text: "vue", link: "/articles/vue/" },
+      { text: "uniapp", link: "" },
       {
-        text: '大前端',
-        items: [
-          { text: "html", link: "/bigFrontEnd/html/" },
-          { text: "css", link: "/bigFrontEnd/css/" },
-          { text: "js", link: "/bigFrontEnd/js/" },
+        text: '博客文档',
+        items: [ // 可以配置成下拉
+          { text: '', link: '' },
         ]
       }
-    ]
-  }
+    ],
+    sidebar: { // 侧边栏，可以分组
+    },
+    socialLinks: [{ icon: "github", link: "https://github.com/czb121/knowledge/tree/master/docs" }], // 可以连接到 github
+  },
 }
+
