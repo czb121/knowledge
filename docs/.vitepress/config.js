7caf16bf99ba57a9056d76f311cf3898ee1cbf92
chenzhenbing <1377196580@qq.com>
2025-05-06 20:57:05 +0800

    fix: 更新配置文件中的 base 路径以指向知识库

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,14 +2,14 @@
  * @Author: chenzhenbing 1377196580@qq.com
  * @Date: 2025-05-04 12:36:19
  * @LastEditors: chenzhenbing 1377196580@qq.com
- * @LastEditTime: 2025-05-05 12:52:46
+ * @LastEditTime: 2025-05-06 20:55:23
  * @FilePath: \study\docs\.vitepress\config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 export default {
   title: '子非鱼', // 博客的标题
   description: '', // 博客的介绍
-  base: '/blog/', // 如果想用 https://mlyz.wdy.github.io/blog/ 访问，那么这句话必填
+  base: '/knowledge/', // 如果想用 https://mlyz.wdy.github.io/blog/ 访问，那么这句话必填
   themeConfig: {
     logo: "", // 页面上显示的logo
     nav: [ // 页面右上角的导航
